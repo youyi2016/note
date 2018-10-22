e515f810759f37c0b018dc3d814f1a70e3e9dca7
youyi <youyi@forchange.tech>
2018-10-22 10:15:41 +0800

    添加转账页面

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const loginRouter = require('./routes/login')
 const transferRouter = require('./routes/transfer')
 const cheatRouter = require('./routes/cheat')
 
-
 app.set('views', path.join(__dirname, 'views'))// 设置存放模板文件的目录
 app.set('view engine', 'ejs')// 设置模板引擎为 ejs
 
--- routes/transfer.js
+++ routes/transfer.js
@@ -2,16 +2,27 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
+    res.render('transfer', {
+
+		})	
+})
+
+router.get('/pay', function(req, res) {
 	const queryObj = req.query
 	const userId = queryObj && queryObj.account
 	const money = queryObj && queryObj.money
 	console.log(JSON.stringify(queryObj))
+	const referer = req.headers.referer
+	// console.log(referer)
+	if(!referer) {
+		res.send('it is not reliable link')
+    return
+	}
 	if(req.cookies.token === '123456789') {
 		res.send('transfer to '+ ' '+ userId + ', success')
 	}else {
 		res.send('transfer fail')
 	}
-	
 })
 
 module.exports = router
