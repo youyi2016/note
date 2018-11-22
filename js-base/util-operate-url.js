2b93ae0174314b18a66ec78e3060b3594e919865
youyi <youyi@forchange.tech>
2018-11-22 10:51:41 +0800

    update 操作url工具方法

---
+++ js-base/util-operate-url.js
@@ -0,0 +1,43 @@
+
+function addQueryToUrl(key, value, type='location') {
+  let url = '';
+  type == 'location' ? url = location.href: url =location.hash
+
+  if(url.indexOf('?') === -1) {
+    url = `${url}?${key}=${value}`
+  }
+  else {
+    const index = url.indexOf(key)
+    if( index === -1) {
+      url = url+'&'+ `${key}=${value}`
+    } else {
+      // const re = "(\\?|^|&|\#)" +key+ "=([^&|^#]*)(&|$|#)"
+      // url = url.replace(/(\\?|^|&|\#)type=([^&|^#]*)(&|$|#)/, "$1"+key+"="+value);
+      const re = `(\\?|&|\#)${key}([^&|^#]*)(&|$|#)`
+      url = url.replace(new RegExp(re), "$1"+key+"="+value+'$3');
+    }
+  }
+  if(type == 'location') {
+    location.href = url
+  }
+
+  if(type == 'pushState') {
+    history.pushState(
+      {},
+      "",
+      url
+    );
+  }
+}
+
+function getUrlQuery() {
+  const qs = location.href.indexOf('?') === -1 ? '':location.href.split('?')[1]
+  const queryParams = qs ? qs.split('&') : []
+  const queryValues = {}
+  queryParams.map(item=> {
+    let values = []
+    item.indexOf('=') !== -1 && (values = item.split('='))
+    queryValues[values[0]] = values[1]
+  })
+  return queryValues
+}
