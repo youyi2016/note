89812c507b0e32d839b7a772a846092d2849270b
youyi <youyi@forchange.tech>
2018-11-25 14:39:59 +0800

    update 获取地址栏参数&修改地址栏参数工具方法

--- js-base/util-operate-url.js
+++ js-base/util-operate-url.js
@@ -1,29 +1,26 @@
-
-function addQueryToUrl(key, value, type='location') {
+function addQueryToUrl(key, value, type = 'location') {
   let url = '';
-  type == 'location' ? url = location.href: url =location.hash
+  type == 'location' ? url = location.href : url = location.hash
 
-  if(url.indexOf('?') === -1) {
+  if (url.indexOf('?') === -1) {
     url = `${url}?${key}=${value}`
-  }
-  else {
+  } else {
     const index = url.indexOf(key)
-    if( index === -1) {
-      url = url+'&'+ `${key}=${value}`
+    if (index === -1) {
+      url = url + '&' + `${key}=${value}`
     } else {
       // const re = "(\\?|^|&|\#)" +key+ "=([^&|^#]*)(&|$|#)"
       // url = url.replace(/(\\?|^|&|\#)type=([^&|^#]*)(&|$|#)/, "$1"+key+"="+value);
       const re = `(\\?|&|\#)${key}([^&|^#]*)(&|$|#)`
-      url = url.replace(new RegExp(re), "$1"+key+"="+value+'$3');
+      url = url.replace(new RegExp(re), "$1" + key + "=" + value + '$3');
     }
   }
-  if(type == 'location') {
+  if (type == 'location') {
     location.href = url
   }
 
-  if(type == 'pushState') {
-    history.pushState(
-      {},
+  if (type == 'pushState') {
+    history.pushState({},
       "",
       url
     );
@@ -31,13 +28,20 @@ function addQueryToUrl(key, value, type='location') {
 }
 
 function getUrlQuery() {
-  const qs = location.href.indexOf('?') === -1 ? '':location.href.split('?')[1]
+  const qs = location.href.indexOf('?') === -1 ? '' : location.href.split('?')[1]
   const queryParams = qs ? qs.split('&') : []
   const queryValues = {}
-  queryParams.map(item=> {
+  queryParams.map(item => {
     let values = []
     item.indexOf('=') !== -1 && (values = item.split('='))
     queryValues[values[0]] = values[1]
   })
   return queryValues
+}
+
+function GetQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
 }
